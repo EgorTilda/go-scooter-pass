0627947f4de7fbdf75ab464424b4f74a5039c857
Egor <egortilda@gmail.com>
2023-11-05 19:44:33 +0300

    modal

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,15 @@
+import modalToggle from './modules/modal';
 const swiper = new Swiper('.swiper', {
     navigation: {
         nextEl: '.gallery-slider__btn--next',
         prevEl: '.gallery-slider__btn--prev',
     },
+});
+
+document.getElementById('cta-btn').addEventListener('click', modalToggle);
+document.getElementById('close-modal').addEventListener('click', modalToggle);
+document.body.addEventListener('keydown', (e)=> {
+    if (e.key == 'Escape') {
+        modalToggle();
+    }
 });
---
+++ src/js/modules/modal.js
@@ -0,0 +1,6 @@
+const modalToggle = () => {
+    document.getElementById('modal').classList.toggle('show');
+    document.body.classList.toggle('show-modal');
+};
+
+export default modalToggle;
